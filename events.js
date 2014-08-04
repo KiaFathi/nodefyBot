d30be2f9394185bcea958adf6ba35c627feccfe5
Kia Fathi <kiafathi@me.com>
2014-08-04 08:56:37 -0700

    Update events.js to work on deployed server

--- events.js
+++ events.js
@@ -1,12 +1,19 @@
+'use strict';
 var request = require('request');
-var keys = require('./API_KEYS_GIT_IGNORE_THIS.js');
 var bodyParser = require('body-parser');
 var Q = require('q');
 
+var keys; 
+
+if(!process.env.PORT){
+  keys = require('./API_KEYS_GIT_IGNORE_THIS.js');
+}
+
+var eventbriteToken = process.env.eventbriteToken || keys.eventbrite.token;
 
 var events = {
   getEventForResponse: function(callback) {
-    var url = 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=San+Francisco&venue.region=CA&token=' + keys.eventbrite.token;
+    var url = 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=San+Francisco&venue.region=CA&token=' + eventbriteToken;
 
     return request(url, function(response, body) {
         var result = JSON.parse(body.body);
