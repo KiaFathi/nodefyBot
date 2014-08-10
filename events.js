a91700eed01ce46dd883eeb38fce90f9f52a60e3
forrestbthomas <forrestbthomas@gmail.com>
2014-08-10 10:33:28 -0700

    Add location specific event response

--- events.js
+++ events.js
@@ -12,8 +12,21 @@ if(!process.env.PORT){
 var eventbriteToken = process.env.eventbriteToken || keys.eventbrite.token;
 
 var events = {
-  getEventForResponse: function(callback) {
-    var url = 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=San+Francisco&venue.region=CA&token=' + eventbriteToken;
+  getEventForResponse: function(location, callback) {
+    if (!location){
+      var url = 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=San+Francisco&venue.region=CA&token=' + eventbriteToken;
+    } else {
+      console.log('LOCATION:', location)
+      var cityState = location.split(', ');
+      var city = cityState[0];
+      var state = cityState[1];
+      if (city.indexOf(' ') !== -1){
+        city = city.split(' ').join('+');
+      }
+      console.log('CITY:', city);
+      console.log('STATE:', state);
+      var url = 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=' + city + '&venue.region=' + state + '&token=' + eventbriteToken;
+    }
 
     return request(url, function(response, body) {
         var result = JSON.parse(body.body);
